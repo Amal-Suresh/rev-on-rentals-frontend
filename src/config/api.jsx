3a78265bb40d9568252829f7bbc4776651adb256
Amal-Suresh <amalsureshms2001@gmail.com>
2023-12-03 19:05:10 +0530

    api changed

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,12 +1,12 @@
-// export const userApi ="https://rev-on.site/"
-// export const adminApi ="https://rev-on.site/admin"
-// export const partnerApi ="https://rev-on.site/partner"
-// export const socketApi ="https://rev-on.site/"
+export const userApi ="https://rev-on.site/"
+export const adminApi ="https://rev-on.site/admin"
+export const partnerApi ="https://rev-on.site/partner"
+export const socketApi ="https://rev-on.site/"
 
 
-export const userApi ="http://localhost:5000/"
-export const adminApi ="http://localhost:5000/admin"
-export const partnerApi ="http://localhost:5000/partner"
-export const socketApi ="http://localhost:5000/"
+// export const userApi ="http://localhost:5000/"
+// export const adminApi ="http://localhost:5000/admin"
+// export const partnerApi ="http://localhost:5000/partner"
+// export const socketApi ="http://localhost:5000/"
 
 
